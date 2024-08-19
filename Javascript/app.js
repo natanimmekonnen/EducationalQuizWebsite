83efc126d3175458c4d2cc6615a6ad4b024767de
Nahom Mekonnen <namek2729@gmail.com>
2024-08-19 12:28:19 +0300

    Final changes

--- Javascript/app.js
+++ Javascript/app.js
@@ -26,3 +26,39 @@ document.getElementById("course").onmousemove = (e) => {
     card.style.setProperty("--mouse-y", `${y}px`);
   }
 };
+
+document.getElementById("contact").onmousemove = (e) => {
+  for (const card of document.getElementsByClassName("contacts")) {
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
+};
+
+let i = 0;
+
+function nextNumber() {
+  i = (i % 3) + 1;
+}
+
+const footer = document.getElementById("footer");
+setInterval(() => {
+  const names = [
+    "../assets/footer1.svg",
+    "../assets/footer2.svg",
+    "../assets/footer3.svg",
+  ];
+
+  footer.style.backgroundImage = `url(${names[i - 1]})`;
+  footer.style.transition = "2s ease-in-out";
+  nextNumber();
+}, 3000);
+
+const login = document.getElementById("login");
+
+// login.addEventListener("click", () => {
+//   login.style.display = "none";
+// });
